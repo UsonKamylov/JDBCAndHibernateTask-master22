a88e7f571ba20d29cc7830a03f0bde5d0cb53ce4
uson <usonkamylov0@gmail.com>
2023-03-31 21:48:14 -0700

    commit

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,28 @@
 package peaksoft;
 
+import peaksoft.dao.UserDaoJdbcImpl;
+import peaksoft.model.User;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+//        Util.connection().close();
+        UserDaoJdbcImpl u=new UserDaoJdbcImpl();
+//   u.createUsersTable();
+//    u.dropUsersTable();
+       u.saveUser("Asan","Taiirov", (byte) 24);
+       u.saveUser("Malika","Moloshova", (byte) 18);
+       u.saveUser("Bakyt","Latibov", (byte) 22);
+       u.saveUser("Nurisa","Jarmamatova", (byte) 20);
+       u.saveUser("Uson","Kamylov", (byte) 23);
+//        u.removeUserById(5);
+//        for (User user : u.getAllUsers()) {
+////
+//            System.out.println(user);
+//        }
+//        u.cleanUsersTable();
+
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/service/UserServiceImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/service/UserServiceImpl.java
@@ -1,31 +1,36 @@
 package peaksoft.service;
 
+import peaksoft.dao.UserDaoJdbcImpl;
 import peaksoft.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJdbcImpl u=new UserDaoJdbcImpl();
 
     public void createUsersTable() {
-
+        u.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        u.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
 
+    public void saveUser(String name, String lastName, Byte age) {
+        u.saveUser("Kanat","Subanov",(byte)23);
     }
+
 
     public void removeUserById(long id) {
-
+        u.removeUserById((long) id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return u.getAllUsers();
     }
 
     public void cleanUsersTable() {
+        u.cleanUsersTable();
     }
 }
