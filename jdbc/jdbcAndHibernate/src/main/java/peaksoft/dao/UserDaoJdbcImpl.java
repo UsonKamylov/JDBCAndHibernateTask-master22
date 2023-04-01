e602e13c56d96edea8aaadd6257f22dcc68b1e50
uson <usonkamylov0@gmail.com>
2023-03-31 21:48:14 -0700

    commit

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
@@ -1,36 +1,110 @@
 package peaksoft.dao;
 
 import peaksoft.model.User;
+import peaksoft.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJdbcImpl implements UserDao {
 
     public UserDaoJdbcImpl() {
-
     }
 
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public void createUsersTable() {
+        String SQL = "CREATE TABLE IF NOT EXISTS users(" +
+                "id SERIAL PRIMARY KEY," +
+                "name VARCHAR(50) NOT NULL," +
+                "lastName VARCHAR(50) NOT NULL," +
+                "age SMALLINT);";
+        try (Connection connection = Util.connection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(SQL);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void dropUsersTable() {
+        String SQL = "DROP TABLE IF EXISTS users";
+        try (Connection con = Util.connection();
+             Statement statement = con.createStatement()) {
+            statement.executeUpdate(SQL);
+            System.out.println(ANSI_RED + "TABLE DELETED" + ANSI_RESET);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String SQL = "INSERT INTO users(name, lastName, age)" +
+                "VALUES(?,?,?)";
+        try (Connection connection = Util.connection();
+             PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void removeUserById(long id) {
+        String SQL = "DELETE FROM users WHERE id=?";
+        try (Connection con = Util.connection();
+             PreparedStatement statement = con.prepareStatement(SQL)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String SQL = "SELECT * FROM users";
+        List<User> users = new ArrayList<>();
+        try (Connection con = Util.connection();
+             Statement statement = con.createStatement()) {
+            ResultSet rs = statement.executeQuery(SQL);
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setLastName(rs.getString("lastName"));
+                user.setAge(rs.getByte("age"));
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        String SQL = "TRUNCATE TABLE users";
+        try (Connection con = Util.connection();
+             Statement statement = con.createStatement()) {
+            statement.executeUpdate(SQL);
+            System.out.println(ANSI_GREEN+"CLEAN TABLED"+ANSI_RESET);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,25 @@
 package peaksoft.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER_NAME = "uson";
+    private static final String PASSWORD = "090909kg";
+
+
+    public static Connection connection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println("Connection to the postgresql  successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
 }
